0d52961ca3f011ba6ec94ebbe0ed220e5d6531e3
kuronuma <pyonpieren@gmail.com>
2025-03-08 19:47:38 +0900

    オートマウスレイヤの状態遷移修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -64,23 +64,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
-    
-    #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
-    if (get_highest_layer(remove_auto_mouse_layer(state, true) != 0))
-    {
-      state = remove_auto_mouse_layer(state, false);
-      set_auto_mouse_enable(false);
-    }
-    else
-    {
-      /* レイヤ０でのみ動作させる */
-      set_auto_mouse_enable(true);
-    }
-    #endif
-    
-    return state;
+  // Auto enable scroll mode when the highest layer is 3
+  keyball_set_scroll_mode(get_highest_layer(state) == 3);
+
+  #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
+  switch(get_highest_layer(remove_auto_mouse_layer(state, true))) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+          state = remove_auto_mouse_layer(state, false);
+          set_auto_mouse_enable(false);
+          break;
+      default:
+          set_auto_mouse_enable(true);
+          break;
+  }
+  #endif
+  
+  return state;
 }
 
 #ifdef OLED_ENABLE
