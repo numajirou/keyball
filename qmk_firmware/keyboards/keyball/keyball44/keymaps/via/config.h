c9f0324bfd3115eb2b7ebce1196df19e2f8e5b40
kuronuma <pyonpieren@gmail.com>
2025-03-09 02:37:05 +0900

    CPI設定のリファクタリング

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
@@ -36,13 +36,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 // #define POINTING_DEVICE_AUTO_MOUSE_ENABLE 
-#define AUTO_MOUSE_DEFAULT_LAYER    6
-#define AUTO_MOUSE_TIME             900  // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
+// #define AUTO_MOUSE_DEFAULT_LAYER    6
+// #define AUTO_MOUSE_TIME             900  // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
 
 #define KEYBALL_CPI_DEFAULT        700  // マウス速度 (default: 500)
 #define KEYBALL_SCROLL_DIV_DEFAULT 5    // スクロール速度 (default: 4)
 #define TAPPING_TERM               180  //  レイヤ切り替わり速度 ミリ秒
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7    // レイヤ数
 
-#define PRECISION_ENABLE // CPI速度調整有効化
-#define PRECISION_CPI 3  // 下げた時のCPI (1/100の値を指定。左記ならCPI 400)
+#define PRECISION_ENABLE                // CPI速度調整有効化
+#define PRECISION_CPI               3  // 下げた時のCPI (1/100の値を指定)
--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/cpiChange.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/cpiChange.c
@@ -2,20 +2,15 @@
 
 #include QMK_KEYBOARD_H
 
-static const uint16_t PROGMEM down_cpi = PRECISION_CPI;
-
-static uint16_t latest_cpi = 1;
-static bool     cpi_state  = false;
+static const uint16_t PROGMEM nomal_cpi = KEYBALL_CPI_DEFAULT / 100;
+static const uint16_t PROGMEM down_cpi  = PRECISION_CPI;
 
 // キー押下中だけ切り替え
 void precision_switch(bool pressed) {
     if (pressed) {
-        if(!cpi_state) {
-            latest_cpi = keyball_get_cpi();
-        }
         keyball_set_cpi(down_cpi);
     } else {
-        keyball_set_cpi(latest_cpi);
+        keyball_set_cpi(nomal_cpi);
     }
 }
 
